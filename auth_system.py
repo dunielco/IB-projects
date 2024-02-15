217967c484cfe45a9fbdebced7227ec52070322f
dunielco <114190608+dunielco@users.noreply.github.com>
2024-02-16 02:07:09 +0300

    Add files via upload

---
+++ auth_system.py
@@ -0,0 +1,374 @@
+import sys, os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QGridLayout, QLabel, QLineEdit, QFileDialog, QMessageBox, QListWidget, QInputDialog
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+
+username_to = ""
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Система авторизации")
+        self.setFixedSize(400, 130)
+
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
+
+        self.mainLayout = QVBoxLayout()
+        self.centralWidget.setLayout(self.mainLayout)
+
+        self.gridLayout = QGridLayout()
+        self.mainLayout.addLayout(self.gridLayout)
+
+        self.usernameLabel = QLabel("Логин:")
+        self.gridLayout.addWidget(self.usernameLabel, 0, 0)
+
+        self.usernameLineEdit = QLineEdit()
+        self.gridLayout.addWidget(self.usernameLineEdit, 0, 1)
+
+        self.passwordLabel = QLabel("Пароль:")
+        self.gridLayout.addWidget(self.passwordLabel, 1, 0)
+
+        self.passwordLineEdit = QLineEdit()
+        self.passwordLineEdit.setEchoMode(QLineEdit.Password)
+        self.gridLayout.addWidget(self.passwordLineEdit, 1, 1)
+
+        self.loginButton = QPushButton("Войти")
+        self.gridLayout.addWidget(self.loginButton, 2, 1)
+
+        self.exitButton = QPushButton("Выйти")
+        self.gridLayout.addWidget(self.exitButton, 3, 1)
+
+        self.messageBox = QMessageBox()        
+        self.messageBox.setWindowTitle(" ")
+
+        self.loginButton.clicked.connect(self.login)
+        self.exitButton.clicked.connect(self.exit)
+        self.count = 0
+
+    def login(self):
+        global username_to
+        username = self.usernameLineEdit.text()
+        password = self.passwordLineEdit.text()
+        with open("users.txt", "r") as f:
+            for line in f:
+                username_check = line.split(",")[0]
+                password_check = line.split(",")[1]
+                blocked_check = line.split(",")[2]
+
+                if username == username_check and password == password_check and blocked_check == "True":
+                    QMessageBox.warning(self, "Внимание!", "Вы заблокированы!")
+                    self.count = 0
+                    break
+
+                if username == "ADMIN" and username == username_check and password == password_check:
+                    QMessageBox.information(self, "Успех!", "Вы вошли как администратор")
+                    self.showAdminInterface()
+                    self.close()
+                    self.count = 0
+                    break
+
+                if username == username_check and username != "ADMIN" and password == password_check:
+                    QMessageBox.information(self, "Успех!", "Добро пожаловать, " + username + "!")
+                    username_to = username
+                    self.showUserInterface()
+                    self.close()
+                    self.count = 0
+                    
+                    break
+            else:
+                QMessageBox.critical(self, "Внимание!", "Неверные данные!")
+                self.count += 1
+                if self.count == 3:
+                    self.close()
+
+    def exit(self):
+        self.close()
+
+    def showAdminInterface(self):
+        self.adminWindow = AdminWindow()
+        self.adminWindow.show()
+
+    def showUserInterface(self):
+        self.userWindow = UserWindow()
+        self.userWindow.show()
+
+class AdminWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Профиль администратора")
+        self.setFixedSize(600, 400)
+
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
+
+        self.mainLayout = QVBoxLayout()
+        self.centralWidget.setLayout(self.mainLayout)
+
+        self.gridLayout = QGridLayout()
+        self.mainLayout.addLayout(self.gridLayout)
+
+        self.userListLabel = QLabel("Список пользователей:")
+        self.gridLayout.addWidget(self.userListLabel, 0, 0)
+
+        self.userListWidget = QListWidget()
+        self.gridLayout.addWidget(self.userListWidget, 1, 0)
+
+        self.addUserButton = QPushButton("Добавить пользователя")
+        self.gridLayout.addWidget(self.addUserButton, 2, 0)
+
+        self.removeUserButton = QPushButton("Удалить пользователя")
+        self.gridLayout.addWidget(self.removeUserButton, 3, 0)
+
+        self.blockUserButton = QPushButton("Заблокировать пользователя")
+        self.gridLayout.addWidget(self.blockUserButton, 4, 0)
+
+        self.unblockUserButton = QPushButton("Разблокировать пользователя")
+        self.gridLayout.addWidget(self.unblockUserButton, 5, 0)
+
+        self.changeLengthButton = QPushButton("Изменить длину пароля")
+        self.gridLayout.addWidget(self.changeLengthButton, 6, 0)
+
+        self.changePasswordButton = QPushButton("Сменить свой пароль")
+        self.gridLayout.addWidget(self.changePasswordButton, 7, 0)
+
+        self.exitButton = QPushButton("Выйти")
+        self.gridLayout.addWidget(self.exitButton, 8, 0)
+
+        self.addUserButton.clicked.connect(self.addUser)
+        self.removeUserButton.clicked.connect(self.removeUser)
+        self.blockUserButton.clicked.connect(self.blockUser)
+        self.unblockUserButton.clicked.connect(self.unblockUser)
+        self.changeLengthButton.clicked.connect(self.changeLength)
+        self.changePasswordButton.clicked.connect(self.changePassword)
+        self.exitButton.clicked.connect(self.exit)
+
+        self.loadUserList()
+
+        with open("users.txt", "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                if line.split(",")[0] == "ADMIN":                    
+                    if line.split(",")[1] == "":
+                        self.changePasswordFirst()
+                        break
+
+    def loadUserList(self):
+        self.userListWidget.clear()
+        with open("users.txt", "r") as f:
+            for line in f:
+                username, password, blocked, length = line.strip().split(",")
+                user_info = username + "," + blocked + "," + length
+                self.userListWidget.addItem(user_info)
+
+    def addUser(self):
+        username, ok = QInputDialog.getText(self, "Добавление пользователя", "Введите имя:")
+        error = False
+        if ok:
+            with open("users.txt", "r") as f:                
+                for line in f:
+                    if username == line.split(",")[0]:
+                        QMessageBox.warning(self, "Ошибка", "Пользователь уже существует.")
+                        error = True
+                        break                    
+            if error == False:        
+                with open("users.txt", "a") as a:
+                    a.write(f"{username},,False,0\n")
+
+        self.loadUserList()
+
+    def removeUser(self):
+        username = self.userListWidget.currentItem().text().split(",")[0]
+        if username == "ADMIN":
+            QMessageBox.warning(self, "Ошибка", "Пользователя нельзя удалить!")
+            return
+        reply = QMessageBox.question(self, "Подтверждение", "Вы уверены, что хотите удалить пользователя {}?".format(username), QMessageBox.Yes | QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            with open("users.txt", "r") as f:
+                lines = f.readlines()
+            with open("users.txt", "w") as f:
+                for line in lines:
+                    if line.split(",")[0] != username:
+                        f.write(line)
+        
+        self.loadUserList()
+
+    def blockUser(self):
+        username = self.userListWidget.currentItem().text().split(",")[0]
+        if username == "ADMIN":
+            QMessageBox.warning(self, "Ошибка", "Пользователя нельзя заблокировать!")
+            return
+        reply = QMessageBox.question(self, "Подтверждение", "Вы уверены, что хотите заблокировать пользователя {}?".format(username), QMessageBox.Yes | QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            with open("users.txt", "r") as f:
+                lines = f.readlines()
+            with open("users.txt", "w") as f:
+                for line in lines:
+                    if line.split(",")[0] == username:
+                        line = line.replace("False", "True")
+                    f.write(line)
+
+        self.loadUserList()
+
+    def unblockUser(self):
+        username = self.userListWidget.currentItem().text().split(",")[0]
+        reply = QMessageBox.question(self, "Подтверждение", "Вы уверены, что хотите разблокировать пользователя {}?".format(username), QMessageBox.Yes | QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            with open("users.txt", "r") as f:
+                lines = f.readlines()
+            with open("users.txt", "w") as f:
+                for line in lines:
+                    if line.split(",")[0] == username:
+                        line = line.replace("True", "False")
+                    f.write(line)
+        self.loadUserList()
+
+    def changeLength(self):
+        username = self.userListWidget.currentItem().text().split(",")[0]
+        length, ok = QInputDialog.getText(self, "Изменение длины пароля", "Введите новую длину:")
+        if ok:
+            with open("users.txt", "r") as f:
+                lines = f.readlines()
+            with open("users.txt", "w") as f:
+                for line in lines:
+                    if line.split(",")[0] == username:
+                        length_old = line.split(",")[3]
+                        if int(length) >= 0 and length != "":                            
+                            line = line.replace(f",{length_old}", f",{length}\n")             
+                            QMessageBox.information(self, "Успех!", "Длина пароля изменена.")
+                        else:
+                            QMessageBox.warning(self, "Ошибка", "Неверная длина пароля.")
+                    f.write(line)
+        self.loadUserList()
+
+
+    def changePasswordFirst(self):
+        error = False
+        username = "ADMIN"
+        password, ok = QInputDialog.getText(self, "Смена пароля", "Введите новый пароль:")
+        if ok:
+            with open("users.txt", "r") as f:
+                lines = f.readlines()
+            with open("users.txt", "w") as f:
+                for line in lines:
+                    if line.split(",")[0] == username:
+                        if len(password) > int(line.split(",")[3]):
+                            line = line.replace(",,", f",{password},")                            
+                            QMessageBox.information(self, "Успех!", "Пароль изменен.")                               
+                        else:
+                            QMessageBox.warning(self,"Слишком короткий пароль!", "Пароль долже быть длиной более " + line.split(",")[3] + " символа(ов)")
+                            error = True
+                    f.write(line)
+        if error == True:
+            exit() 
+
+    def changePassword(self):
+        username = "ADMIN"
+        password_old, ok = QInputDialog.getText(self, "Смена пароля", "Введите текущий пароль:")
+        if ok:
+            with open("users.txt", "r") as f:
+                lines = f.readlines()
+            with open("users.txt", "w") as f:
+                for line in lines:
+                    if line.split(",")[0] == username:
+                        current_password = line.split(",")[1]
+                        if current_password == password_old:
+                            password_new, ok = QInputDialog.getText(self, "Смена пароля", "Введите новый пароль:")
+                            if len(password_new) > int(line.split(",")[3]):
+                                line = line.replace(f",{password_old},", f",{password_new},")                            
+                                QMessageBox.information(self, "Успех!", "Пароль изменен.")
+                            else:
+                                QMessageBox.warning(self,"Слишком короткий пароль!", "Пароль долже быть длиной более " + line.split(",")[3] + " символа(ов)")
+                        else:
+                            QMessageBox.warning(self, "Ошибка", "Неверный текущий пароль.")
+                        f.write(line)
+
+    def exit(self):
+        self.close()
+
+class UserWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        with open("users.txt", "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                if line.split(",")[0] == username_to:                    
+                    if line.split(",")[1] == "":
+                        self.changePasswordFirst()
+                        break
+
+        self.setWindowTitle("Профиль пользователя")
+        self.setFixedSize(200, 100)
+
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
+
+        self.mainLayout = QVBoxLayout()
+        self.centralWidget.setLayout(self.mainLayout)
+
+        self.gridLayout = QGridLayout()
+        self.mainLayout.addLayout(self.gridLayout)
+
+        self.changePasswordButton = QPushButton("Сменить свой пароль")
+        self.gridLayout.addWidget(self.changePasswordButton, 1, 0)
+
+        self.exitButton = QPushButton("Выйти")
+        self.gridLayout.addWidget(self.exitButton, 2, 0)
+
+        self.changePasswordButton.clicked.connect(self.changePassword)
+        self.exitButton.clicked.connect(self.exit)
+
+    def exit(self):
+        self.close()
+
+    def changePasswordFirst(self):
+        error = False
+        password, ok = QInputDialog.getText(self, "Смена пароля", "Введите новый пароль:")
+        if ok:
+            with open("users.txt", "r") as f:
+                lines = f.readlines()
+            with open("users.txt", "w") as f:
+                for line in lines:
+                    if line.split(",")[0] == username_to:
+                        if len(password) > int(line.split(",")[3]):
+                            line = line.replace(",,", f",{password},")                            
+                            QMessageBox.information(self, "Успех!", "Пароль изменен.")                               
+                        else:
+                            QMessageBox.warning(self,"Слишком короткий пароль!", "Пароль долже быть длиной более " + line.split(",")[3] + " символа(ов)")
+                            error = True
+                    f.write(line)
+        if error == True:
+            exit() 
+
+    def changePassword(self):
+        password_old, ok = QInputDialog.getText(self, "Смена пароля", "Введите текущий пароль:")
+        if ok:
+            with open("users.txt", "r") as f:
+                lines = f.readlines()
+            with open("users.txt", "w") as f:
+                for line in lines:
+                    if line.split(",")[0] == username_to:
+                        current_password = line.split(",")[1]
+                        if current_password == password_old:
+                            password_new, ok = QInputDialog.getText(self, "Смена пароля", "Введите новый пароль:")
+                            if len(password_new) > int(line.split(",")[3]):
+                                line = line.replace(f",{password_old},", f",{password_new},")                            
+                                QMessageBox.information(self, "Успех!", "Пароль изменен.")
+                            else:
+                                QMessageBox.warning(self,"Слишком короткий пароль!", "Пароль долже быть длиной более " + line.split(",")[3] + " символа(ов)")
+                        else:
+                            QMessageBox.warning(self, "Ошибка", "Неверный текущий пароль.")
+                        f.write(line)
+
+if __name__ == "__main__":
+    if not os.path.exists("users.txt"):
+        with open("users.txt", "w") as f:
+            f.write(f"ADMIN,,False,0\n")
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
